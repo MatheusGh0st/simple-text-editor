37cb5b96f6d114cb84462483851ac8fbd22d6bcb
MatheusGh0st <matheusghost88@gmail.com>
2023-10-29 12:55:54 -0300

    take args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+fn main() -> Result<(), io::Error> {
+    let args: Vec<String> = env.args().collect();
+
+    if args.len() < 2 {
+        println!("Usage: {} <file>", args[0]);
+        std::process::exit(1);
+    }
+
+    let filename = &args[1];
 }
