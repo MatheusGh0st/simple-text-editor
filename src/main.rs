0c20567442eb900128dfc047c46c0e67b571a067
MatheusGh0st <matheusghost88@gmail.com>
2023-10-29 13:15:20 -0300

    new imports and adding options of modified file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::process;
 use std::io::{Error};
 
 fn main() -> Result<(), Error> {
@@ -6,10 +7,34 @@ fn main() -> Result<(), Error> {
 
     if args.len() < 2 {
         println!("Usage: {} <file>", args[0]);
-        std::process::exit(1);
+        process::exit(1);
     }
 
     let filename = &args[1];
+
+    if !fs::metadata(filename).is_ok() {
+        println!("Error file don't exist!");
+        process::exit(1);
+    }
+
+    loop {
+        println!("Enter a command |i: insert, r: read, d: delete, q: quit|: ");
+        let mut input = String::new();
+        io::stdin().read_line(&mut input);
+
+        match input.trim() {
+            "i" => {}
+            "r" => {}
+            "d" => {}
+            "q" => {
+                println!("Exit");
+                break;
+            }
+            _ => {
+                println!("Invalid command| Try again");
+            }
+        }
+    }
 
     Ok(())
 }
